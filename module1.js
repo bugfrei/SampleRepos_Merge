057b1c1f7706fbfbd57cb36c3901a73c137008a5
Carsten Schlegel <cschle@gmail.com>
2022-08-20 00:21:14 +0200

    Gerade Zahlen

--- module1.js
+++ module1.js
@@ -1,7 +1,10 @@
+// Kollege arbeitet an diesem Modul
 console.log("Das ist Modul 1");
 
 for (var i = 0; i < 10; i++)
 {
     console.log(i);
 }
+
+console.log("Das sind die Zahlen von 0 bis 9);
 
---
+++ module3.js
@@ -0,0 +1,10 @@
+// Kollege hat schonmal mit Modul 3 angefangen
+
+console.log("Das ist Module 3");
+for (var i = 0; i < 20; i+=2)
+{
+    console.log("Gerade Zahl: " + i.toString());
+}
+
+console.log("Fertig");
+
