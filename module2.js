be0af0e97c3fb967c373c2b4079243a5478ce8db
Carsten Schlegel <cschle@gmail.com>
2022-08-20 00:25:44 +0200

    Ungerade Zahlen

--- module2.js
+++ module2.js
@@ -1,7 +1,11 @@
+// Hans arbeiter an diesem Modul
 console.log("Das ist Modul 2");
 
 for (var i = 10; i < 20; i++)
 {
-    console.log(i);
+    console.log("Zahl: " + i.toString());
 }
+
+console.log("Das waren die Zahlen von Module 2");
+
 
---
+++ module3.js
@@ -0,0 +1,10 @@
+// Hans hat schonmal mit Modul 3 angefangen
+
+console.log("Das ist Module 3");
+for (var i = 1; i < 20; i+=2)
+{
+    console.log("Ungerade Zahl: " + i.toString());
+}
+
+console.log("Fertig");
+
