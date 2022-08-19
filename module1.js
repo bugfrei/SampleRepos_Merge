1e94f83db44895e69fe8c4d5cc422e836fbff132
Carsten Schlegel <cschle@gmail.com>
2022-08-20 00:18:35 +0200

    Initial

---
+++ module1.js
@@ -0,0 +1,7 @@
+console.log("Das ist Modul 1");
+
+for (var i = 0; i < 10; i++)
+{
+    console.log(i);
+}
+
---
+++ module2.js
@@ -0,0 +1,7 @@
+console.log("Das ist Modul 2");
+
+for (var i = 10; i < 20; i++)
+{
+    console.log(i);
+}
+
